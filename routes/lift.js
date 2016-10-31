b4dc9410c46067c3969d9412ff711b25c200126c
John D. Gaffney <john.gaffney@isobar.com>
2016-10-31 13:53:45 -0400

    INIT: added back views

--- routes/lift.js
+++ routes/lift.js
@@ -9,14 +9,27 @@ router.get('/', function(req, res, next) {
 router.get('/squat', function(req, res, next) {
 
     //TODO pull data from data file and render the template
-    var deadlift = [
-        { date: "10-03-2016", lbs: 155 },
-        { date: "10-10-2016", lbs: 225 },
-        { date: "10-17-2016", lbs: 240 },
-        { date: "10-24-2016", lbs: 255 },
-        { date: "10-31-2016", lbs: 270 }
+    var liftVolumes = [
+        { date: "10-03-2016", squat: 155, deadlift: 155, bench: null, press: null },
+        { date: "10-05-2016", squat: 165, deadlift: null, bench: null, press: null },
+        { date: "10-10-2016", squat: 170, deadlift: 155, bench: null, press: null },
+        { date: "10-12-2016", squat: 175, deadlift: null, bench: null, press: null },
+        { date: "10-17-2016", squat: 180, deadlift: 155, bench: null, press: null },
+        { date: "10-19-2016", squat: 185, deadlift: null, bench: null, press: null },
+        { date: "10-24-2016", squat: 190, deadlift: 155, bench: null, press: null },
+        { date: "10-26-2016", squat: 195, deadlift: null, bench: null, press: null },
+        { date: "10-31-2016", squat: 200, deadlift: 155, bench: null, press: null },
+        { date: "11-02-2016", squat: null, deadlift: null, bench: null, press: null },
+        { date: "11-07-2016", squat: null, deadlift: 155, bench: null, press: null },
+        { date: "11-09-2016", squat: null, deadlift: null, bench: null, press: null },
+        { date: "11-14-2016", squat: null, deadlift: 155, bench: null, press: null },
+        { date: "11-16-2016", squat: null, deadlift: null, bench: null, press: null },
+        { date: "11-21-2016", squat: null, deadlift: 155, bench: null, press: null },
+        { date: "11-23-2016", squat: null, deadlift: null, bench: null, press: null },
+        { date: "11-28-2016", squat: null, deadlift: 155, bench: null, press: null },
+        { date: "11-30-2016", squat: null, deadlift: null, bench: null, press: null }
     ];
-    res.render('line', { liftArray: deadlift });
+    res.render('lift', { liftArray: liftVolumes });
 });
 
 module.exports = router;
